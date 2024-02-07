74fff1c4f850886fcd7b0500a8dce0585c8d0e09
mchib031 <71799789+mchib031@users.noreply.github.com>
2024-02-07 18:40:58 -0500

    implemented profile initial design

--- App.js
+++ App.js
@@ -30,23 +30,29 @@ function Search() {
 }
 
 function Profile() {
-  /*const user={
-    username:'ashemartha',
-    level:52
-  };
-  return(
-    <SafeAreaView style={styles.container}>
-      <Text>Username: {user.username}</Text>
-      <Text>Level: {user.level}</Text>
-    </SafeAreaView>
-  );*/
   return(
     <View style={styles.profileContainer} >
       <View style={styles.balanceContainer}>
-          <Text>WWW</Text>         
+          <Text>balance</Text>         
       </View>
       <View style={styles.photoContainer}>
-
+        <Text>photo</Text>
+      </View>
+      <Text>username#id</Text>
+      <Text style={styles.accountLabel}>ACCOUNT</Text>
+      <View style={styles.stickyStat}>
+        <View style={[styles.statContainer, {width: 260 }]}>
+          <Text>RANK</Text>
+        </View>
+        <View style={[styles.statContainer, {width: 200 }]}>
+          <Text>LEVEL</Text>
+        </View>
+        <View style={[styles.statContainer, {width: 140 }]}>
+          <Text>TOTAL SPENT</Text>
+        </View>
+      </View>
+      <View style={styles.favouriteAgent}>
+          <Text>Favourite Agent</Text>
       </View>
     </View>
   );
@@ -77,7 +83,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     margin:20,
-    
     borderStyle:'solid',
     borderRadius:20,
     shadowColor: "#000",
@@ -88,20 +93,59 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.34,
     shadowRadius: 6.27,
     elevation: 10,
-    
+    justifyContent:'flex-start',
+    alignItems:'center',
   },
   balanceContainer: {
     backgroundColor:'gray',
     marginTop:30,
     width:120,
+    height:40,
+    justifyContent:'center',
+    alignItems:'center',
     borderStyle:'solid',
     borderRadius:20,
     marginLeft:190,
   },
   photoContainer: {
-    backgroundColor:'gray',
+    backgroundColor:'red',
     alignItems:'center',
     textAlign:'center',
+    justifyContent:'center',
+    alignContent:'center',
+    width:120,
+    height:120,
+    borderRadius: 60,
+    marginTop:30,
+  },
+  stickyStat: {
+    position:'sticky',
+    bottom:0,
+    alignSelf: 'flex-start',
+  },
+  statContainer: {
+    backgroundColor:'gray',
+    width:120,
+    borderStyle:'solid',
+    borderTopRightRadius:20,
+    borderBottomRightRadius:20,
+    marginTop:30,
+    height:40,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  accountLabel: {
+    marginTop:20,
+    fontSize:20,
+  },
+  favouriteAgent: {
+    backgroundColor:'gray',
+    width:260,
+    height:120,
+    justifyContent:'center',
+    alignItems:'center',
+    borderStyle:'solid',
+    borderRadius:20,
+    marginTop:30,
   }
-
 });
