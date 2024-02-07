63a8d2c9f979877bde8982c8b776897b9b55b93b
mchib031 <71799789+mchib031@users.noreply.github.com>
2024-02-07 17:24:49 -0500

    updated profile navigation and style, added app logo and menu icons

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import { NavigationContainer} from '@react-navigation/native';
+import { StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator();
 
@@ -45,26 +50,76 @@ function Profile() {
 
 
 export default function App() {
+
+  const logo = require('./assets/logo.png');
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-      <Tab.Navigator>
-          <Tab.Screen name="Profile" component={Profile}  />
-          <Tab.Screen name="Search" component={Search}  />
-          <Tab.Screen name="History" component={History}  />
-        </Tab.Navigator> 
-    </NavigationContainer>
-    </SafeAreaProvider>
-    
+    <View style={styles.container}>
+      <View style={styles.topNav}>
+        <Image source={logo} style ={{ width: 98, height: 30 }}/>
+        <View>
+          <View style={styles.topRightNav}>
+            <Icon style={styles.icon} name="search" size={24} />
+            <Icon style={styles.icon} name="account-circle" size={24}/>
+          </View>
+        </View>
+      </View>
+      <View style={styles.body}>
+
+      </View>
+      <View style={styles.bottomNav}>
+        <TouchableOpacity> 
+          <Icon style={styles.icon} name="home" size={34}/>
+          <Text>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tabButton}> 
+          <Icon style={styles.icon} name="history" size={34}/>
+          <Text>History</Text>
+        </TouchableOpacity>
+        
+      </View>
+    </View>
     
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  topNav: {
+    elevation: 10,
+    height: 70,
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+  topRightNav: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 28,
+  },
+  icon: {
+    color: '#364966',
+  },
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f2f5'
+  },
+  bottomNav: {
+    height: 60,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    borderTopColor: '#E5E5E5',
+    borderStyle: 'solid',
+    borderTopWidth: 0.5,
+  },
+  tabButton: {
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
