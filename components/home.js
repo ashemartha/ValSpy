39ef4e84a79ba1f266175eb5a090aa63d74f0a58
mchib031 <71799789+mchib031@users.noreply.github.com>
2024-02-09 11:45:36 -0500

    implemented agents touchble opacities

--- components/home.js
+++ components/home.js
@@ -1,25 +1,58 @@
 import { StyleSheet,
     Text,
-    View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { SafeAreaView } from 'react-native-safe-area-context';
+    View,
+    ScrollView,
+    Image, 
+    TouchableOpacity} from 'react-native';
 
 export default function Home() {
   
     return(
-      <SafeAreaView style={styles.container}>
-        <View>
-          <ScrollView>
-            <Text>Home</Text>
+        <View style={styles.container}>
+          <ScrollView contentContainerStyle={styles.scrollContainer} alwaysBounceVertical={true}>
+            <Text>Agents</Text>
+            <View style={styles.agentsContainer}>
+              <TouchableOpacity style={styles.agent}>
+                <Image source={require('../assets/favicon.png')} style={{width: 50, height: 50}} />
+                <Text>agent-name</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.agent}>
+                <Image source={require('../assets/favicon.png')} style={{width: 50, height: 50}} />
+                <Text>agent-name</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.agent}>
+                <Image source={require('../assets/favicon.png')} style={{width: 50, height: 50}} />
+                <Text>agent-name</Text>
+              </TouchableOpacity>
+            </View>
           </ScrollView>
         </View>
-      </SafeAreaView>
     );
   }
 
   const styles = StyleSheet.create({
     container: {
       flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    scrollContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: 20,
+    },
+    agentsContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    agent: {
+      margin: 10,
+      padding: 10,
+      backgroundColor: 'lightgrey',
+      borderRadius: 10,
       justifyContent: 'center',
       alignItems: 'center',
     }
